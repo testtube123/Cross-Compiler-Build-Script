5a1e6ad872f9522e5dcba74306a814589fe3daa4
testtube123 <goodaythatismyname@gmail.com>
2022-01-12 15:56:01 -0500

    Tweaks

--- cross-compiler.sh
+++ cross-compiler.sh
@@ -5,8 +5,6 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
-sudo apt install libgmp3-dev libmpfr-dev libisl-dev libcloog-isl-dev libmpc-dev texinfo -y
-
 cd "$(dirname "$0")"
 
 mkdir out
@@ -17,11 +15,11 @@ mkdir src
 
 cd src
 
-wget https://ftp.gnu.org/gnu/binutils/binutils-2.33.1.tar.xz
-wget ftp://ftp.gnu.org/gnu/gcc/gcc-9.2.0/gcc-9.2.0.tar.xz
+wget https://ftp.gnu.org/gnu/binutils/binutils-2.36.1.tar.xz
+wget ftp://ftp.gnu.org/gnu/gcc/gcc-9.2.0/gcc-11.1.0.tar.xz
 
-tar -xvzf binutils-2.33.1.tar.xz
-tar -xvzf gcc-9.2.0.tar.xz
+tar -xvf binutils-2.36.1.tar.xz
+tar -xvf gcc-11.1.0.tar.xz
 
 export PREFIX="$(pwd)/../path/"
 export TARGET=$1
@@ -29,15 +27,16 @@ export PATH="$PREFIX/bin:$PATH"
 
 mkdir build-binutils
 cd build-binutils
-../binutils-2.33.1/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+../binutils-2.36.1/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
 make -j $(nproc)
 make install
 
 cd ..
 mkdir build-gcc
 cd build-gcc
-../gcc-9.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+../gcc-11.1.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
 make -j $(nproc) all-gcc
 make -j $(nproc) all-target-libgcc
 make install-gcc
 make install-target-libgcc
+
